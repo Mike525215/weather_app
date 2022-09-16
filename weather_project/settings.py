12d249d089ef91dee06fe62c6a52bd18ed4187b4
Volodymyr Popov <volodymyr.popov@megogo.net>
2022-09-16 12:15:17 +0200

    modified settings.py

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -117,9 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+LOGIN_URL = '/login'
 try:
     from .local_settings import *
 except ImportError:
